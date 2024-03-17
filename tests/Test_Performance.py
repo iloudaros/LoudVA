3b45cef9d970c28034c391f19e4a71a325f0977b
Ioannis Loudaros <johnloudaros@hotmail.com>
2024-03-17 16:24:19 +0200

    It now returns the default values after execution

--- tests/Test_Performance.py
+++ tests/Test_Performance.py
@@ -10,7 +10,7 @@ import os
 import ihelper as i
 
 #### Power modes
-power_modes = [0]
+power_modes = [0,1]
 
 for mode in power_modes:
     # Set power mode
@@ -25,10 +25,13 @@ for mode in power_modes:
     print("Renaming the results")
     os.system('mv /home/iloudaros/LoudVA/measurements/performance_measurements.csv /home/iloudaros/LoudVA/measurements/performance_measurements_mode_' + str(mode) + '.csv')
 
+# Return to the default power mode
+print(f"Setting power mode to 0")
+os.system(f"sudo nvpmodel -m 0")
 
 #### GPU Clock Speeds
 # These are the supported frequencies for the GPU on the Jetson Nano
-gpu_freqs = [ 921600000] #[76800000, 153600000, 230400000, 307200000, 384000000, 460800000, 537600000, 614400000, 691200000, 768000000, 844800000,
+gpu_freqs = [76800000, 153600000, 230400000, 307200000, 384000000, 460800000, 537600000, 614400000, 691200000, 768000000, 844800000, 921600000] 
 
 
 # Measure the performance of the system for each frequency using the perf_analyzer tool
@@ -46,4 +49,7 @@ for freq in gpu_freqs:
     os.system('mv /home/iloudaros/LoudVA/measurements/performance_measurements.csv /home/iloudaros/LoudVA/measurements/performance_measurements_freq_' + str(freq) + '.csv')
 
 
-
+# Return the Makefile GPU frequency to the default value and reenable the 3d-scaling
+print(f"Setting GPU frequency to 76800000 and reenabling 3d-scaling")
+i.modify_gpu_freq('/home/iloudaros/LoudVA/makefile', 76800000)
+os.system('cd /home/iloudaros/LoudVA && make 3D_scaling')

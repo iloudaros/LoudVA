ab592d327c389b3bc9a1845ded6b268e663de15a
Ioannis Loudaros <johnloudaros@hotmail.com>
2024-03-25 01:38:56 +0200

    Update performance profiling script and test script

--- tests/Test_Performance.py
+++ tests/Test_Performance.py
@@ -1,41 +1,41 @@
-# ### Performance Test ###
-# # 
-# # We use this notebook to test the effect of the power mode (or GPU clk speed) of the Jetson on the performance of the triton server, with the dynamic batching feature enabled.
-# # - We will use the `perf_analyzer` tool to measure the throughput of the server with different power modes.
-# # - We will use the `nvpmodel` tool to change the power mode of the Jetson.
-# # 
-# # (To be run on the Jetsons via the performance_profiling ansible playbook.)
+### Performance Test ###
+# 
+# We use this notebook to test the effect of the power mode (or GPU clk speed) of the Jetson on the performance of the triton server, with the dynamic batching feature enabled.
+# - We will use the `perf_analyzer` tool to measure the throughput of the server with different power modes.
+# - We will use the `nvpmodel` tool to change the power mode of the Jetson.
+# 
+# (To be run on the Jetsons via the performance_profiling ansible playbook.)
 
-# import os
-# import ihelper as i
+import os
+import ihelper as i
 
-# #### Power modes
-# power_modes = [0,1]
+#### Power modes
+power_modes = [0,1]
 
-# for mode in power_modes:
-#     # Set power mode
-#     print(f"---Setting power mode to {mode}---")
-#     os.system(f"sudo nvpmodel -m {mode}")
+for mode in power_modes:
+    # Set power mode
+    print(f"---Setting power mode to {mode}---")
+    os.system(f"sudo nvpmodel -m {mode}")
 
-#     # Modify the makefile to change the MEASUREMENT_INTERVAL 
-#     if mode == 1:
-#         i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', 10000)
-#     else:
-#         i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', 5000)
+    # Modify the makefile to change the MEASUREMENT_INTERVAL 
+    if mode == 1:
+        i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', 10000)
+    else:
+        i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', 5000)
 
-#     # Run the performance test
-#     print("Running performance test")
-#     os.system('cd /home/iloudaros/LoudVA && make measure_performance_csv')
+    # Run the performance test
+    print("Running performance test")
+    os.system('cd /home/iloudaros/LoudVA && make measure_performance_csv')
 
-#     # Rename the results according to the power mode
-#     print("Renaming the results")
-#     os.system('mv /home/iloudaros/LoudVA/measurements/performance_measurements.csv /home/iloudaros/LoudVA/measurements/performance_measurements_mode_' + str(mode) + '.csv')
+    # Rename the results according to the power mode
+    print("Renaming the results")
+    os.system('mv /home/iloudaros/LoudVA/measurements/performance_measurements.csv /home/iloudaros/LoudVA/measurements/performance_measurements_mode_' + str(mode) + '.csv')
 
-# ### 
-# # Return to the default power mode
-# print(f"---Setting power mode to 0---")
-# os.system(f"sudo nvpmodel -m 0")
-# ###
+### 
+# Return to the default power mode
+print(f"---Setting power mode to 0---")
+os.system(f"sudo nvpmodel -m 0")
+###
 
 #### GPU Clock Speeds
 # These are the supported frequencies for the GPU on the Jetson Nano
@@ -50,10 +50,10 @@ for freq in gpu_freqs:
     os.system('sleep 5')
     i.modify_gpu_freq('/home/iloudaros/LoudVA/makefile', freq)
 
-    # Modify the makefile to change the MEASUREMENT_INTERVAL 
-    new_interval = (921600000//freq)*5000+1000
-    print(f"Setting MEASUREMENT_INTERVAL to {new_interval}")
-    i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', new_interval)
+    # Modify the makefile to change the MEASUREMENT_INTERVAL (not needed because we changed the mode to count_windows for more info read the makefile)
+    # new_interval = (921600000//freq)*5000+1000
+    # print(f"Setting MEASUREMENT_INTERVAL to {new_interval}")
+    # i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', new_interval)
 
     # Run the performance measurement from the makefile
     print("Running performance test")

14ba7229929323af8f107e3e2aa2bf2e49064a0c
Ioannis Loudaros <johnloudaros@hotmail.com>
2024-03-24 19:15:38 +0200

    only freqs again

--- tests/Test_Performance.py
+++ tests/Test_Performance.py
@@ -1,41 +1,41 @@
-### Performance Test ###
-# 
-# We use this notebook to test the effect of the power mode (or GPU clk speed) of the Jetson on the performance of the triton server, with the dynamic batching feature enabled.
-# - We will use the `perf_analyzer` tool to measure the throughput of the server with different power modes.
-# - We will use the `nvpmodel` tool to change the power mode of the Jetson.
-# 
-# (To be run on the Jetsons via the performance_profiling ansible playbook.)
+# ### Performance Test ###
+# # 
+# # We use this notebook to test the effect of the power mode (or GPU clk speed) of the Jetson on the performance of the triton server, with the dynamic batching feature enabled.
+# # - We will use the `perf_analyzer` tool to measure the throughput of the server with different power modes.
+# # - We will use the `nvpmodel` tool to change the power mode of the Jetson.
+# # 
+# # (To be run on the Jetsons via the performance_profiling ansible playbook.)
 
-import os
-import ihelper as i
+# import os
+# import ihelper as i
 
-#### Power modes
-power_modes = [0,1]
+# #### Power modes
+# power_modes = [0,1]
 
-for mode in power_modes:
-    # Set power mode
-    print(f"---Setting power mode to {mode}---")
-    os.system(f"sudo nvpmodel -m {mode}")
+# for mode in power_modes:
+#     # Set power mode
+#     print(f"---Setting power mode to {mode}---")
+#     os.system(f"sudo nvpmodel -m {mode}")
 
-    # Modify the makefile to change the MEASUREMENT_INTERVAL 
-    if mode == 1:
-        i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', 10000)
-    else:
-        i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', 5000)
+#     # Modify the makefile to change the MEASUREMENT_INTERVAL 
+#     if mode == 1:
+#         i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', 10000)
+#     else:
+#         i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', 5000)
 
-    # Run the performance test
-    print("Running performance test")
-    os.system('cd /home/iloudaros/LoudVA && make measure_performance_csv')
+#     # Run the performance test
+#     print("Running performance test")
+#     os.system('cd /home/iloudaros/LoudVA && make measure_performance_csv')
 
-    # Rename the results according to the power mode
-    print("Renaming the results")
-    os.system('mv /home/iloudaros/LoudVA/measurements/performance_measurements.csv /home/iloudaros/LoudVA/measurements/performance_measurements_mode_' + str(mode) + '.csv')
+#     # Rename the results according to the power mode
+#     print("Renaming the results")
+#     os.system('mv /home/iloudaros/LoudVA/measurements/performance_measurements.csv /home/iloudaros/LoudVA/measurements/performance_measurements_mode_' + str(mode) + '.csv')
 
-### 
-# Return to the default power mode
-print(f"---Setting power mode to 0---")
-os.system(f"sudo nvpmodel -m 0")
-###
+# ### 
+# # Return to the default power mode
+# print(f"---Setting power mode to 0---")
+# os.system(f"sudo nvpmodel -m 0")
+# ###
 
 #### GPU Clock Speeds
 # These are the supported frequencies for the GPU on the Jetson Nano

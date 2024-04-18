63de1985bab7f8d045bba1269340f38151ede1d7
Ioannis Loudaros <johnloudaros@hotmail.com>
2024-04-19 01:55:42 +0300

    Profiling for AGX and NX (demo)

--- tests/Test_AGX_Performance.py
+++ tests/Test_AGX_Performance.py
@@ -1,1 +1,100 @@
-print("Me lene akribi esena pos se lene")
+#!/usr/bin/python3 -u
+### Performance Test ###
+# 
+# We use this script to test the effect of the power mode (or GPU clk speed) of the Jetson on the performance of the triton server, with the dynamic batching feature enabled.
+# - We will use the `perf_analyzer` tool to measure the throughput of the server with different power modes.
+# - We will use the `nvpmodel` tool to change the power mode of the Jetson.
+# 
+# (To be run on the Jetsons via the performance_profiling ansible playbook.)
+
+import os
+import ihelper as i
+maximum_concurrency = 13
+
+#### Power modes
+# The supported power modes for the Jetson AGX Xavier
+power_modes = [0, 1, 2, 3, 4, 5, 6, 7] 
+
+# Measure the performance of the system for each power mode using the perf_analyzer tool
+for mode in power_modes:
+
+    # Set power mode
+    print(f"---Setting power mode to {mode}---")
+    os.system(f"sudo nvpmodel -m {mode}")
+
+    # For each concurrency level, run the performance test
+    for conc in range(1, maximum_concurrency+1):
+        print(f"---Setting concurrency to {conc}---")
+        i.modify_variable('/home/iloudaros/LoudVA/makefile', 'CONCURRENCY_FLOOR', '=', conc)
+        i.modify_variable('/home/iloudaros/LoudVA/makefile', 'CONCURRENCY_LIMIT', '=', conc)
+
+        # Run the performance test
+        print("Running performance test")
+        os.system('cd /home/iloudaros/LoudVA && make measure_performance_and_power')
+
+        # Rename the results according to the power mode
+        print("Renaming the results")
+        os.system(f'mv /home/iloudaros/LoudVA/measurements/performance/performance_measurements.csv /home/iloudaros/LoudVA/measurements/performance/modes/performance_measurements_mode_{mode}_conc_{conc}.csv')
+        os.system(f'mv /home/iloudaros/LoudVA/measurements/power/power_measurement_stats /home/iloudaros/LoudVA/measurements/power/modes/power_measurement_stats_mode_{mode}_conc_{conc}.csv')
+
+        # Empty the log of tegra_stats
+        os.system('rm /home/iloudaros/LoudVA/measurements/power/tegra_log')
+    
+    # combine the results of the different concurrencies
+    print("Combining the results")
+    os.system(f'cd /home/iloudaros/LoudVA/measurements/performance/modes && bash /home/iloudaros/LoudVA/scripts/combine_measurements.sh performance_measurements_mode_{mode}')
+    os.system(f'cd /home/iloudaros/LoudVA/measurements/power/modes && bash /home/iloudaros/LoudVA/scripts/combine_measurements.sh power_measurement_stats_mode_{mode}')
+
+### 
+# Return to the default power mode
+print(f"---Setting power mode to 0---")
+os.system(f"sudo nvpmodel -m 0")
+###
+
+#### GPU Clock Speeds
+# These are the supported frequencies for the GPU on the Jetson AGX Xavier
+gpu_freqs = [ 114750000, 216750000, 318750000, 420750000, 522750000, 624750000, 675750000, 828750000, 905250000, 1032750000, 1198500000, 1236750000, 1338750000, 1377000000] 
+
+
+# Measure the performance of the system for each frequency using the perf_analyzer tool
+for freq in gpu_freqs:
+
+    # Modify the makefile to change the gpu frequency
+    print(f"---Setting GPU frequency to {freq}---")
+    os.system('sudo jetson_clocks --restore /home/iloudaros/LoudVA/power_management/l4t_dfs.conf')
+    os.system(f"sudo nvpmodel -m 0")
+    os.system('sleep 5')
+    i.modify_gpu_freq(freq)
+
+    # For each concurrency level, run the performance test
+    for conc in range(1, maximum_concurrency+1):
+        print(f"---Setting concurrency to {conc}---")
+        i.modify_variable('/home/iloudaros/LoudVA/makefile', 'CONCURRENCY_FLOOR', '=', conc)
+        i.modify_variable('/home/iloudaros/LoudVA/makefile', 'CONCURRENCY_LIMIT', '=', conc)
+        
+
+       # Run the performance test
+        print("Running performance test")
+        os.system('cd /home/iloudaros/LoudVA && make measure_performance_and_power')
+
+        # Rename the results according to the freq
+        print("Renaming the results")
+        os.system(f'mv /home/iloudaros/LoudVA/measurements/performance/performance_measurements.csv /home/iloudaros/LoudVA/measurements/performance/freqs/performance_measurements_freq_{freq}_conc_{conc}.csv')
+        os.system(f'mv /home/iloudaros/LoudVA/measurements/power/power_measurement_stats /home/iloudaros/LoudVA/measurements/power/freqs/power_measurement_stats_freq_{freq}_conc_{conc}.csv')
+
+        # Empty the log of tegra_stats
+        os.system('rm /home/iloudaros/LoudVA/measurements/power/tegra_log')
+    
+    # combine the results of the different concurrencies
+    print("Combining the results")
+    os.system(f'cd /home/iloudaros/LoudVA/measurements/performance/freqs && bash /home/iloudaros/LoudVA/scripts/combine_measurements.sh performance_measurements_freq_{freq}')
+    os.system(f'cd /home/iloudaros/LoudVA/measurements/power/freqs && bash /home/iloudaros/LoudVA/scripts/combine_measurements.sh power_measurement_stats_freq_{freq}')
+
+
+# Return the Makefile GPU frequency and MEASUREMENT_INTERVAl to the default value, reenable the 3d-scaling
+print(f"Returning to the default values")
+i.modify_variable('/home/iloudaros/LoudVA/makefile', 'MEASUREMENT_INTERVAL', '=', 5000)
+i.modify_variable('/home/iloudaros/LoudVA/makefile', 'GPU_MIN_FREQ', '=', 76800000)
+i.modify_variable('/home/iloudaros/LoudVA/makefile', 'GPU_MAX_FREQ', '=', 921600000)
+os.system('sudo jetson_clocks --restore /home/iloudaros/LoudVA/power_management/l4t_dfs.conf')
+os.system('sudo nvpmodel -m 0')

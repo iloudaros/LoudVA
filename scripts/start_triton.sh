0f66be6558acf9cbe3177c465c786edcba86761e
Ioannis Loudaros <johnloudaros@hotmail.com>
2024-04-26 20:09:57 +0300

    very important change

--- scripts/start_triton.sh
+++ scripts/start_triton.sh
@@ -1,2 +1,2 @@
 sudo sh -c 'cd /home/iloudaros/tritonserver/ && nohup ./bin/tritonserver  --model-repository=./model_repository --backend-directory=./backends --backend-config=tensorflow,version=2 &'
-sleep 5
+sleep 2

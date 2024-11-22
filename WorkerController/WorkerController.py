6186bb5cece16468fa238e25dfd2687d501677fd
Ioannis Loudaros <johnloudaros@hotmail.com>
2024-11-22 04:45:28 +0200

    fixed

--- WorkerController/WorkerController.py
+++ WorkerController/WorkerController.py
@@ -3,23 +3,20 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "running"
-
 @app.route('/set_gpu_freq/<int:freq>', methods=['GET'])
 def set_gpu_freq(freq):
     try:
-        # Execute the bash script with the provided frequency
-        result = subprocess.run(['sudo ../scripts/shell/set_gpu_freq.sh', str(freq)], capture_output=True, text=True, check=True)
-        
-        # Return the script's output
+        # Run the shell script with the provided frequency
+        result = subprocess.run(
+            ['/home/iloudaros/LoudVA/scripts/shell/set_frequency.sh', str(freq)],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,  # Use this instead of text=True
+            check=True
+        )
         return jsonify({"message": result.stdout}), 200
-
     except subprocess.CalledProcessError as e:
-        # Return the error if the script fails
         return jsonify({"error": e.stderr}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
